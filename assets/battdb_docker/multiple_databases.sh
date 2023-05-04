21828871175c355a07b57b90fff6b32ac61988d9
Bing Syuan Wang <bingw@battgenie.life>
2023-05-03 23:11:15 -0700

    update init script

--- assets/battdb_docker/multiple_databases.sh
+++ assets/battdb_docker/multiple_databases.sh
@@ -13,12 +13,6 @@ function create_user_and_database() {
 EOSQL
 }
 
-echo "hihihi"
-
-echo "$POSTGRES_MULTIPLE_DATABASES"
-echo "$POSTGRES_USER"
-echo "$POSTGRES_PASSWORD"
-
 if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
 	echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
 	for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
